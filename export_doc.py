c77ccf599519b42352081be910a77d735d358a8b
Max Cohen <max.cohen@gmx.com>
2019-12-04 12:24:57 +0100

    Add parser to export_doc script

--- export_doc.py
+++ export_doc.py
@@ -1,25 +1,37 @@
 import json
 import datetime
 from pathlib import Path
+import argparse
 
-notebook_path = Path("training_classic.ipynb")
-export_dir = Path("docs/source/notebooks/")
+def export_notebook(notebook_path: Path, export_dir: Path):
+    # Load notebook
+    with open(notebook_path, "r") as stream_json:
+        notebook = json.load(stream_json)
 
-# Load notebook
-with open(notebook_path, "r") as stream_json:
-    notebook = json.load(stream_json)
+    # Get original title
+    title = notebook['cells'][0]['source'][0]
 
-# Get original title
-title = notebook['cells'][0]['source'][0]
+    # Add date to title
+    export_time = datetime.datetime.now()
+    notebook['cells'][0]['source'][0] = f'{title} - {export_time.strftime("%Y %B %d")}'
 
-# Add date to title
-export_time = datetime.datetime.now()
-notebook['cells'][0]['source'][0] = f'{title} - {export_time.strftime("%Y %B %d")}'
+    # Add date to export path
+    export_name = f'training_{export_time.strftime("%Y_%m_%d__%H%M%S")}.ipynb'
+    export_path = export_dir.joinpath(export_name)
 
-# Add date to export path
-export_name = f'{notebook_path.stem}_{export_time.strftime("%Y_%m_%d__%H%M%S")}.ipynb'
-export_path = export_dir.joinpath(export_name)
+    # Export
+    with open(export_path, "w") as stream_json:
+        json.dump(notebook, stream_json)
 
-# Export
-with open(export_path, "w") as stream_json:
-    json.dump(notebook, stream_json)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Export notebook to docs.')
+
+    parser.add_argument('notebook')
+    parser.add_argument('-o', '--output-dir', )
+
+    args = parser.parse_args()
+    
+    notebook_path = Path(args.notebook)
+    export_path = Path(args.output_dir) or Path("docs/source/notebooks/")
+
+    export_notebook(notebook_path, export_path)
