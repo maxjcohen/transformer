62bf3a4333477815f94c699916e88fbea24d5c61
max cohen <max.cohen@gmx.com>
2020-01-04 17:03:16 +0100

    Fix master file for doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../tst/'))
 
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
