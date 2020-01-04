3ec50ad1afb9412403a806438e32a8454b30a577
max cohen <max.cohen@gmx.com>
2020-01-04 16:33:04 +0100

    Add package install scripts

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tst",
+    version="0.1.0",
+    author="Max Cohen",
+    author_email="max.zagouri@pm.me",
+    description="Time Series Transformer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://timeseriestransformer.readthedocs.io/",
+    packages=['tst'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
