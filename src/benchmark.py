47a4f543fe4afbf8d78d9e35dffcd5f83180eae0
Max Cohen <max.zagouri@pm.me>
2020-03-16 09:19:55 +0100

    Add GRU for benchmark

--- src/benchmark.py
+++ src/benchmark.py
@@ -28,7 +28,7 @@ class LSTM(nn.Module):
                  **kwargs):
         super().__init__(**kwargs)
 
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=num_layers, dropout=dropout)
+        self.rnn = nn.LSTM(input_dim, hidden_dim, num_layers=num_layers, dropout=dropout)
         self.linear = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -43,6 +43,34 @@ class LSTM(nn.Module):
         -------
             Output tensor with shape (m, K, output_dim)
         """
-        lstm_out, _ = self.lstm(x)
-        output = self.linear(lstm_out)
+        rnn_out, _ = self.rnn(x)
+        output = self.linear(rnn_out)
         return output
+
+class GRU(LSTM):
+    """Benchmark GRU.
+
+    Parameters
+    ----------
+    input_dim:
+        Input dimension.
+    hidden_dim:
+        Latent dimension.
+    output_dim:
+        Output dimension.
+    num_layers:
+        Number of GRU layers.
+    dropout:
+        Dropout value. Default is ``0``.
+    """
+
+    def __init__(self,
+                 input_dim: int,
+                 hidden_dim: int,
+                 output_dim: int,
+                 num_layers: int,
+                 dropout: float = 0,
+                 **kwargs):
+        super().__init__(input_dim, hidden_dim, output_dim, num_layers, dropout, **kwargs)
+
+        self.rnn = nn.GRU(input_dim, hidden_dim, num_layers=num_layers, dropout=dropout)
