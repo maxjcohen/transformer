c8253e71c519c5ebacc6f782db3ca8a840862a82
Max Cohen <max.zagouri@pm.me>
2020-04-14 10:09:37 +0200

    Add ConvGru model to benchmark

--- src/benchmark.py
+++ src/benchmark.py
@@ -84,3 +84,42 @@ class BiGRU(LSTM):
         super().__init__(input_dim, hidden_dim, output_dim, num_layers, dropout, bidirectional, **kwargs)
 
         self.rnn = nn.GRU(input_dim, hidden_dim, num_layers=num_layers, dropout=dropout, batch_first=True, bidirectional=bidirectional)
+
+
+class ConvGru(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 hidden_dim: int,
+                 output_dim: int,
+                 num_layers: int,
+                 dropout: float = 0,
+                 bidirectional: bool = False,
+                 **kwargs):
+        super().__init__(**kwargs)
+
+        self.conv1 = nn.Conv1d(in_channels=input_dim, out_channels=hidden_dim, kernel_size=11, stride=1, padding=11//2)
+        self.conv2 = nn.Conv1d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=11, stride=1, padding=11//2)
+        self.conv3 = nn.Conv1d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=11, stride=1, padding=11//2)
+
+        self.activation = nn.LeakyReLU(0.1)
+
+        self.rnn = BiGRU(hidden_dim,
+                         hidden_dim,
+                         output_dim,
+                         num_layers,
+                         dropout=dropout,
+                         bidirectional=bidirectional)
+
+    def forward(self, x):
+        x = x.transpose(1, 2)
+        x = self.conv1(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+        x = self.activation(x)
+        x = self.conv3(x)
+        x = self.activation(x)
+        x = x.transpose(1, 2)
+
+        x = self.rnn(x)
+
+        return x
