de92c1ad749344155d0ca1be22403aac8bbb2f4b
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-05-03 15:58:41 -0300

    :heavy_minus_sign: Remove csv2npz and make_npz.py

--- make_npz.py
+++
@@ -1,7 +0,0 @@
-"""
-Create dataset.npz from x and y train .csv files.
-"""
-from src.utils.csv2npz import csv2npz
-
-csv2npz('datasets/x_train_LsAZgHU.csv', 'datasets/y_train_EFo1WyE.csv', 'datasets', \
- 'dataset_CAPT_v7')
