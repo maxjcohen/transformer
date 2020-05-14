85ebab12842d044bc4f72d0b9fb828c5537dd008
Max Cohen <max.zagouri@pm.me>
2020-05-14 11:27:04 +0200

    Extend requirements to python 3.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     install_requires=[
         "torch",
     ]
