906d883aae51797d8edcd35114ba90bc2dd6b6da
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-05-29 00:22:47 -0300

    :rocket: Speed up transformer pytest.

--- tests/main_test.py
+++ tests/main_test.py
@@ -27,10 +27,18 @@ def test_transformer_tsp():
     """
     Tests the TransformerTimeSeriesPredictor
     """
-    tsp = TransformerTimeSeriesPredictor()
+    tsp = TransformerTimeSeriesPredictor(
+        epochs=1,
+        attention_size=3,
+        number_of_coders=2,
+        query_size=2,
+        value_size=2,
+        d_model=16,
+        number_of_heads=2
+    )
     dataset = OzeNPZDataset(dataset_path=npz_check(
         Path('datasets'), 'dataset'), labels_path="labels.json")
 
     tsp.fit(dataset, loss_function=OZELoss(alpha=0.3))
     mean_loss = tsp.compute_mean_loss(tsp.dataloader)
-    assert mean_loss < 0.01
+    assert mean_loss < 0.05
