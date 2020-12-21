78e00c4ed7ee215fe5ec6aad7d5f3a54abd4740d
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-12-21 02:19:04 +0000

    :white_check_mark: Update main_test.

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,11 +2,15 @@
 main test script
 To run issue the command pytest at the root folder of the project.
 """
+import time
+from datetime import timedelta
+
+import pytest
 import torch
 from flights_time_series_dataset import FlightsDataset
-import pytest
 from time_series_predictor import TimeSeriesPredictor
 from time_series_transformer import Transformer
+
 
 def test_transformer_tsp():
     """
@@ -15,6 +19,7 @@ def test_transformer_tsp():
     if not torch.cuda.is_available():
         pytest.skip("needs a CUDA compatible GPU available to run this test")
 
+    start = time.time()
     tsp = TimeSeriesPredictor(
         Transformer(),
         max_epochs=50,
@@ -24,11 +29,15 @@ def test_transformer_tsp():
     tsp.fit(FlightsDataset())
     score = tsp.score(tsp.dataset)
     assert score > -1
+    end = time.time()
+    elapsed = timedelta(seconds = end - start)
+    print("Fitting in GPU time delta: {}".format(elapsed))
 
 def test_transformer_tsp_in_cpu():
     """
     Tests the TransformerTimeSeriesPredictor
     """
+    start = time.time()
     tsp = TimeSeriesPredictor(
         Transformer(),
         max_epochs=50,
@@ -39,3 +48,6 @@ def test_transformer_tsp_in_cpu():
     tsp.fit(FlightsDataset())
     score = tsp.score(tsp.dataset)
     assert score > -1
+    end = time.time()
+    elapsed = timedelta(seconds = end - start)
+    print("Fitting in CPU time delta: {}".format(elapsed))
