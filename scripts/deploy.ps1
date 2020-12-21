c8de4459130d6d814a123744d510d5a8dc9909f7
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-12-21 01:36:36 +0000

    Bump version: 0.4.1 → 0.4.2

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,5 +1,5 @@
 python setup.py sdist bdist_wheel
-$version="0.4.1"
+$version="0.4.2"
 $files_to_handle_str="dist/time_series_transformer-$version*" 
 twine check $files_to_handle_str
 twine upload $files_to_handle_str
