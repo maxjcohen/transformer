6e46a9b0039b80a8d40e612fef41fb8451fe2feb
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-05 20:31:42 +0000

    :art: Simplified test code with pytest parametrize.

--- tests/main_test.py
+++ tests/main_test.py
@@ -11,10 +11,11 @@ from flights_time_series_dataset import FlightsDataset
 from time_series_predictor import TimeSeriesPredictor
 from tst import Transformer
 
-# @pytest.mark.skip
-def test_transformer_tsp():
-    if not torch.cuda.is_available():
-        pytest.skip("needs a CUDA compatible GPU available to run this test")
+@pytest.mark.parametrize('device', ['cuda', 'cpu'])
+def test_transformer_tsp(device):
+    if device == 'cuda':
+        if not torch.cuda.is_available():
+            pytest.skip("needs a CUDA compatible GPU available to run this test")
 
     start = time.time()
     tsp = TimeSeriesPredictor(
@@ -28,21 +29,4 @@ def test_transformer_tsp():
     assert score > -1
     end = time.time()
     elapsed = timedelta(seconds = end - start)
-    print("Fitting in GPU time delta: {}".format(elapsed))
-
-# @pytest.mark.skip
-def test_transformer_tsp_in_cpu():
-    start = time.time()
-    tsp = TimeSeriesPredictor(
-        Transformer(),
-        max_epochs=50,
-        train_split=None,
-        device='cpu',
-    )
-
-    tsp.fit(FlightsDataset())
-    score = tsp.score(tsp.dataset)
-    assert score > -1
-    end = time.time()
-    elapsed = timedelta(seconds = end - start)
-    print("Fitting in CPU time delta: {}".format(elapsed))
+    print("Fitting in {} time delta: {}".format(device, elapsed))
