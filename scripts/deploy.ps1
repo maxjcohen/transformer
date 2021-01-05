5351d2120c85bca6722e9eaf0831117419e45c6d
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-05 15:58:19 +0000

    :fire: Remove bumpversion cfg file.

--- scripts/deploy.ps1
+++
@@ -1,5 +0,0 @@
-python setup.py sdist bdist_wheel
-$version="0.4.3"
-$files_to_handle_str="dist/time_series_transformer-$version*" 
-twine check $files_to_handle_str
-twine upload $files_to_handle_str
