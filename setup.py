0064064507322c38c1da573716518aac9853c4e3
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-05 16:05:28 +0000

    Rebase setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-<<<<<<< HEAD
     name="tst",
     version="0.3.0",
     author="Max Cohen",
     author_email="max.zagouri@pm.me",
-=======
-    name="time_series_transformer",
-    version="1.0.0",
-    author="Daniel Kaminski de Souza",
-    author_email="daniel@kryptonunite.com",
->>>>>>> 27a9c3b58c74a1217403b8e008d275bd0e8ac3c5
     description="Time Series Transformer",
     long_description=long_description,
     long_description_content_type="text/markdown",
